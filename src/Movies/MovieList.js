aaf5b0e5f2be494d24cf39d3416c1300e7bb91d5
Torien3x <tstanley0323@gmail.com>
2023-10-23 23:05:09 -0700

    React Router Completion

--- src/Movies/MovieList.js
+++ src/Movies/MovieList.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function MovieList(props) {
+export default function MovieList({movies, setId}) {
+
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map(movie => (
+        <MovieDetails key={movie.id} movie={movie}/>
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
+
   const { title, director, metascore } = props.movie;
 
   return (
+    <Link to={`movie/${props.movie.id}`}>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -22,6 +26,6 @@ function MovieDetails(props) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
-    </div>
+    </div></Link>
   );
 }
